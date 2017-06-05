99526e1d5486f031565d3b71e5a299971505575e
Oana Maier <oana.maier@yahoo.com>
2017-06-05 21:47:38 +0300

    Added style for App and removed id from array movies.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
 import Movies from './Constants/movies.js';
+import './App.css';
 
 var movies = Movies.data1;
-class App extends Component {
+ class App extends Component {
 
     render() {
+    	
         return (
-            <div className="App">
-            	{
-            	movies.map(function(movie){
-            		return (
-            			<div key={movie.id}>
-	            			<h2>{movie.name}</h2>
-	            			<p>Year: {movie.year}</p>
-            			</div>
-            			)
-            		})
-            	}
-            </div>
+        	<div className="container">
+	        	<div>
+	        		<h1>Movies with React</h1>
+	        	</div>
+	        	<div>
+	        		<h2>List of Movies</h2>
+	        	</div>
+	            <div className="app">
+	            	<div className="row">
+		            	<div className="col-md-6">
+		            		<form>
+					            <h3>Looking for a specific movie?</h3>
+					            <input type="text" placeholder="Search.." />
+				            </form>
+		            	</div>
+		            </div>
+		            	{
+		            	movies.map(function(movie, index){
+		            		//movies.filter(searchingFor(term)).map(function(movie, index){
+		            		return (
+		            		<div className="row">
+		  
+			            		<div className="col-md-6">
+			            			<div className="list" key={index}>
+				            			<span>{movie.name}</span>
+				            			<p>Year: {movie.year}</p>
+			            			</div>
+			            		</div>
+			            		
+		            		</div>
+		            			
+		            			);
+		            		})
+		            	}
+	            </div>
+	        </div>
         );
     }
 }
--- src/Constants/movies.js
+++ src/Constants/movies.js
@@ -2,14 +2,14 @@
  * Created by Oana on 5/28/2017.
  */
 const moviesData = [
-	{id: 1, name: "World War Z", year: "2013"},
-	{id: 2, name: "Salt", year: "2010"},
-	{id: 3, name: "50/50", year: "2011"},
-	{id: 4, name: "Shrek", year: "2013"},
-	{id: 5, name: "Prometheus", year: "2012"},
-	{id: 6, name: "The Prestige", year: "2006"},
-	{id: 7, name: "A Series of Unfortunate Events", year: "2004"},
-	{id: 8, name: "Up", year: "2009"}
+	{name: "World War Z", year: "2013"},
+	{name: "Salt", year: "2010"},
+	{name: "50/50", year: "2011"},
+	{name: "Shrek", year: "2013"},
+	{name: "Prometheus", year: "2012"},
+	{name: "The Prestige", year: "2006"},
+	{name: "A Series of Unfortunate Events", year: "2004"},
+	{name: "Up", year: "2009"}
 	]
 
 	module.exports = {data1: moviesData}
